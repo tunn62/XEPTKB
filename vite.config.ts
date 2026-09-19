6f349ac3d809e59dd9ad93b0d08c52db9a4e1877
tunn62 <tunn62@gmail.com>
2026-09-19 07:52:11 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    Base:'/XEPTKB/',
+    Base:'/TKBtieuhoc/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
